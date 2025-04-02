404726811306ea2acd074e37c912b384af4cd5a7
shubhendu ghosh <shubhendu-ghosh@users.noreply.huggingface.co>
2025-04-02 14:25:13 +0000

    Update app/main.py

--- app/main.py
+++ app/main.py
@@ -19,4 +19,4 @@ def generate():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=7860, debug=True)
